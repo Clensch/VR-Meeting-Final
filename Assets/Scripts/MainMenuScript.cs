82a592e188ab5b1ee4958c603ffc28c90ef9d035
Clensch <Christopher.Lensch@googlemail.com>
2021-03-23 18:32:08 +0100

    Update MainMenuScript.cs

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -39,12 +39,6 @@ public class MainMenuScript : MonoBehaviour
 
     public void EndProgramm()
     {
-        if (UnityEditor.EditorApplication.isPlaying)
-        {
-            UnityEditor.EditorApplication.isPlaying = false;
-        } else
-        {
-            Application.Quit();
-        }
+        Application.Quit();
     }
 }
